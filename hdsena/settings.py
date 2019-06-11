32537336c88f7cab334557b93681f5c4b9549df5
unknown <User@DESKTOP-C05KG9B.SENA.RED>
2019-06-11 11:05:42 -0500

    finalfinal2

--- hdsena/settings.py
+++ hdsena/settings.py
@@ -90,7 +90,8 @@ WSGI_APPLICATION = 'hdsena.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""DATABASES = {
+"""
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
