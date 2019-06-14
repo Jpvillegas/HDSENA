14a6fbe959d19c0f68fbefeaa6c7aac1021854a7
unknown <User@DESKTOP-C05KG9B.SENA.RED>
2019-06-14 12:15:48 -0500

    navfinal

--- hdsena/settings.py
+++ hdsena/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yqg5rtu5%o!g&_1kl(6_ov_xq1$!=3_@q6l5k1vt=syhqem9s#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
